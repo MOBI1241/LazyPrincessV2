7f73ea9ec9e20720df730cda90480311194d8201
MOBI1241 <158516807+MOBI1241@users.noreply.github.com>
2024-02-01 22:54:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MOBI1241/LazyPrincessV2/tree/main /MOBI1241
+  git clone https://github.com/MOBI1241/LazyPrincessV2.git /MOBI1241
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
   git clone $UPSTREAM_REPO /MOBI1241
 fi
-cd /LazyPrincessV2
+
+cd /MOBI1241  # שימוש בשם התיקייה שהתקבלה מ git clone
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
